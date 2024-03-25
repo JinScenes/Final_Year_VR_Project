37c25827a79d2902c1b9efd85b4a69c06a325976
Jin <100594091+JinScenes@users.noreply.github.com>
2024-03-25 20:42:10 +0000

    new update

--- Final VR Project/Assets/Scripts/Weapons/BulletInsert.cs
+++ Final VR Project/Assets/Scripts/Weapons/BulletInsert.cs
@@ -1,12 +1,56 @@
 ﻿using UnityEngine.XR.Interaction.Toolkit;
+using System.Collections;
 using UnityEngine;
 
 public class BulletInsert : MonoBehaviour
 {
     [SerializeField] private RaycastWeapon Weapon;
     [SerializeField] private AudioClip InsertSound;
+    [SerializeField] private Renderer insertRenderer;
+    [SerializeField] private float flashDuration = .5f;
+
+    private Grabbable grabbableComponent;
 
     public string AcceptBulletName = "Bullet";
+    private Coroutine flashRoutine = null;
+
+    private void Awake()
+    {
+        if (insertRenderer != null)
+        {
+            insertRenderer.enabled = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Weapon.GetComponent<Grabbable>().BeingHeld &&
+            Weapon.GetBulletCount() < Weapon.MaxInternalAmmo &&
+            flashRoutine == null)
+        {
+            StartFlashing();
+        }
+        else if (Weapon.GetBulletCount() >= Weapon.MaxInternalAmmo && flashRoutine != null)
+        {
+            StopFlashing();
+        }
+    }
+
+    private void UpdateFlashingStatus()
+    {
+        if (grabbableComponent.BeingHeld &&
+            Weapon.GetBulletCount() < Weapon.MaxInternalAmmo &&
+            flashRoutine == null)
+        {
+            StartFlashing();
+        }
+        else if ((!grabbableComponent.BeingHeld || Weapon.GetBulletCount() >= Weapon.MaxInternalAmmo) &&
+                 flashRoutine != null)
+        {
+            StopFlashing();
+        }
+    }
+
 
     void OnTriggerEnter(Collider other)
     {
@@ -33,9 +77,37 @@ public class BulletInsert : MonoBehaviour
                 {
                     XRManager.Instance.PlaySpatialClipAt(InsertSound, transform.position, 1f, 0.5f);
                 }
+
+                UpdateFlashingStatus();
             }
         }
     }
 
+    private void StartFlashing()
+    {
+        flashRoutine = StartCoroutine(FlashIndicator());
+    }
+
+    private void StopFlashing()
+    {
+        if (flashRoutine != null)
+        {
+            StopCoroutine(flashRoutine);
+            flashRoutine = null;
+        }
+        if (insertRenderer != null)
+        {
+            insertRenderer.enabled = false;
+        }
+    }
+
+    private IEnumerator FlashIndicator()
+    {
+        while (true)
+        {
+            insertRenderer.enabled = !insertRenderer.enabled;
+            yield return new WaitForSeconds(flashDuration);
+        }
+    }
 }
 
