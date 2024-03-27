c5edd3ff96f7a00656ef7a0db77b844a9f00d728
Jin <100594091+JinScenes@users.noreply.github.com>
2024-03-27 23:50:41 +0000

    wow

--- Final VR Project/Assets/Scripts/Weapons/AmmoCountDisplay.cs
+++ Final VR Project/Assets/Scripts/Weapons/AmmoCountDisplay.cs
@@ -7,11 +7,20 @@ public class AmmoCountDisplay : MonoBehaviour
     [SerializeField] private AmmoDispenser ammoDispenser;
     [SerializeField] private string weaponType;
 
+    [SerializeField] private int lowAmmoThreshold = 5;
+    [SerializeField] private int shotgunShellRatio = 30;
+
+    [SerializeField] private Color lowAmmoColor = Color.yellow;
+    [SerializeField] private Color noAmmoColor = Color.red;
+
     private Text displayText;
+    private Color originalColor;
 
     void Start()
     {
         displayText = GetComponent<Text>();
+        originalColor = displayText.color;
+
         if (ammoDispenser == null)
         {
             Debug.LogError("AmmoDispenser reference not set on " + gameObject.name);
@@ -28,32 +37,48 @@ public class AmmoCountDisplay : MonoBehaviour
 
     private void UpdateDisplay()
     {
+        int ammoCount = 0;
         switch (weaponType)
         {
             case "Pistol":
-                displayText.text = "?";
+                ammoCount = int.MaxValue;
                 break;
             case "SCAR":
-                displayText.text = $"{ammoDispenser.CurrentSCARClips}";
+                ammoCount = ammoDispenser.CurrentSCARClips;
                 break;
             case "M4A4":
-                displayText.text = $"{ammoDispenser.CurrentM4A4Clips}";
+                ammoCount = ammoDispenser.CurrentM4A4Clips;
                 break;
             case "AK74U":
-                displayText.text = $"{ammoDispenser.CurrentAK74UClip}";
+                ammoCount = ammoDispenser.CurrentAK74UClip;
                 break;
             case "SIGMCX":
-                displayText.text = $"{ammoDispenser.CurrentSIGMCXClip}";
+                ammoCount = ammoDispenser.CurrentSIGMCXClip;
                 break;
             case "Leader_50":
-                displayText.text = $"{ammoDispenser.CurrentLeader_50Clip}";
+                ammoCount = ammoDispenser.CurrentLeader_50Clip;
                 break;
             case "Shotgun":
-                displayText.text = $"{ammoDispenser.CurrentShotgunShells}";
+                ammoCount = ammoDispenser.CurrentShotgunShells / shotgunShellRatio;
                 break;
             default:
                 displayText.text = "Unknown Weapon Type";
-                break;
+                return;
+        }
+
+        displayText.text = ammoCount == int.MaxValue ? "INF" : ammoCount.ToString();
+
+        if (ammoCount == 0)
+        {
+            displayText.color = noAmmoColor;
+        }
+        else if (ammoCount <= lowAmmoThreshold)
+        {
+            displayText.color = lowAmmoColor;
+        }
+        else
+        {
+            displayText.color = originalColor;
         }
     }
 }
