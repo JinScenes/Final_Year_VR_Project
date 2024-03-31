0d969cd69352d04b9f76682139cc4a260fb01a5f
Jin <100594091+JinScenes@users.noreply.github.com>
2024-03-31 18:09:25 +0100

    all done

--- Final VR Project/Assets/Scripts/Environment/DoorOpen.cs
+++ Final VR Project/Assets/Scripts/Environment/DoorOpen.cs
@@ -1,37 +1,46 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorOpen : MonoBehaviour
 {
-    GameObject player;
-    [SerializeField] GameObject lDoor, rDoor;
+    public GameObject player;
+    public GameObject lDoor, rDoor;
+    public string destroyableTag = "DestroyableWeapon";
+
     void Start()
     {
-       player = GameObject.FindGameObjectWithTag("Player");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == player)
         {
-            StartCoroutine(doorCloseandOpen());
+            StartCoroutine(DoorCloseAndOpen());
         }
     }
 
- private  IEnumerator doorCloseandOpen()
+    private IEnumerator DoorCloseAndOpen()
     {
         lDoor.transform.Rotate(0, -90, 0);
         rDoor.transform.Rotate(0, 90, 0);
+
+        DestroyWeapons();
+
         yield return new WaitForSeconds(5);
-        print("Doors closing");
+
+        Debug.Log("Doors closing");
+
         lDoor.transform.Rotate(0, 90, 0);
         rDoor.transform.Rotate(0, -90, 0);
+    }
+
+    private void DestroyWeapons()
+    {
+        GameObject[] destroyableObjects = GameObject.FindGameObjectsWithTag(destroyableTag);
+        foreach (GameObject obj in destroyableObjects)
+        {
+            Destroy(obj);
+        }
     }
 }
